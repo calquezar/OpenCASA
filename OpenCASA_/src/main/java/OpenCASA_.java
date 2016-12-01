e5fa7a39a3d1d11cfb07d53a212ed4ec199e6132
calquezar <calquezar@USUARIO-PC>
2016-12-01 16:25:05 +0100

    Initial push

---
+++ OpenCASA_/src/main/java/OpenCASA_.java
@@ -0,0 +1,56 @@
+
+
+import ij.ImageJ;
+import ij.ImagePlus;
+import ij.plugin.filter.PlugInFilter;
+import ij.process.ImageProcessor;
+
+
+/**
+ * OpenCASA
+ *
+ * 
+ *
+ * @author Biozar team
+ */
+public class OpenCASA_ implements PlugInFilter {
+	protected ImagePlus image;
+
+	@Override
+	public int setup(String arg, ImagePlus imp) {
+		if (arg.equals("about")) {
+			return DONE;
+		}
+
+		image = imp;
+		return DOES_8G | DOES_16 | DOES_32 | DOES_RGB;
+	}
+
+	@Override
+	public void run(ImageProcessor ip) {
+
+	}
+
+
+	/**
+	 * Main method for debugging.
+	 *
+	 * For debugging, it is convenient to have a method that starts ImageJ, loads an
+	 * image and calls the plugin, e.g. after setting breakpoints.
+	 *
+	 * @param args unused
+	 */
+	public static void main(String[] args) {
+		// set the plugins.dir property to make the plugin appear in the Plugins menu
+		Class<?> clazz = OpenCASA_.class;
+		String url = clazz.getResource("/" + clazz.getName().replace('.', '/') + ".class").toString();
+		String pluginsDir = url.substring("file:".length(), url.length() - clazz.getName().length() - ".class".length());
+		System.setProperty("plugins.dir", pluginsDir);
+
+		// start ImageJ
+		new ImageJ();
+		
+		// run the plugin
+		//IJ.runPlugIn(clazz.getName(), "");
+	}
+}
