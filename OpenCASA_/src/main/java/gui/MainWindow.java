7d03db22ca8a6f47701e6da7923a9341074f28c4
USUARIO <USUARIO@USUARIO-PC>
2016-12-07 13:22:39 +0100

    created MainWindow class on gui package

---
+++ OpenCASA_/src/main/java/gui/MainWindow.java
@@ -0,0 +1,29 @@
+package gui;
+
+import java.awt.GraphicsConfiguration;
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class MainWindow extends JFrame {
+
+	public MainWindow() throws HeadlessException {
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainWindow(GraphicsConfiguration gc) {
+		super(gc);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainWindow(String title) throws HeadlessException {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+
+	public MainWindow(String title, GraphicsConfiguration gc) {
+		super(title, gc);
+		// TODO Auto-generated constructor stub
+	}
+
+}
