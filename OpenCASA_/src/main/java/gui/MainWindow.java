8c82bb22d046f0d9bf5456734c7a343f32e1674e
Carlos <Carlos@ISAAC-PC>
2019-11-16 18:54:49 +0100

    .

--- OpenCASA_/src/main/java/gui/MainWindow.java
+++ OpenCASA_/src/main/java/gui/MainWindow.java
@@ -147,7 +147,6 @@ public class MainWindow extends JFrame {
           } catch (Exception e1) {
             IJ.handleException(e1);
           }
-          
         } else if (label.equals("Concentration")) {
           CellCountParams.setGlobalParams();
           mw.setVisible(false);
